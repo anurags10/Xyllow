ee4bf824713a7aaccf8b721679c7160ebae0f0f6
ANURAG SINGH <iamanuragsingh10@gmail.com>
2024-05-26 16:58:12 +0530

    second commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+// configuring solidity version
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.17", 
 };
